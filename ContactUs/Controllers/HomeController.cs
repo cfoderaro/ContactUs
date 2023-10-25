9b49c3dcbf3f6ad777faef1de7d7aa1b424d750c
Cole Foderaro <cole.foderaro@gmail.com>
2023-10-25 13:54:58 -0400

    Add project files.

---
+++ ContactUs/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ContactUs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ContactUs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(ContactData contactData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(contactData);
+            }
+            else
+            {
+                // TODO: Write data to file
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ContactUs/Models/ContactData.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactUs.Models
+{
+    public class ContactData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // TODO: Validate email address, custom attribute? MS default?
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+    }
+}
